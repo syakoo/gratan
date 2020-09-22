e88d833012dca8ceeb6df1491115d22dd8157923
syakoo <nichijou4009@gmail.com>
2020-09-22 11:03:24 +0900

    update: change an order of the color selection

--- src/components/SelectedItem/ColorSelector.tsx
+++ src/components/SelectedItem/ColorSelector.tsx
@@ -13,17 +13,17 @@ type ColorSelector = {
 const ColorSelector: React.FC<ColorSelector> = ({ value, onChange }) => {
   return (
     <Select value={value} onChange={(e) => onChange(e.target.value)}>
+      <option value="black">black</option>
+      <option value="white">white</option>
+      <option value="gray">gray</option>
+      <option value="darkgray">darkgray</option>
+      <option value="lightgray">lightgray</option>
       <option value="red">red</option>
       <option value="green">green</option>
       <option value="blue">blue</option>
       <option value="cyan">cyan</option>
       <option value="magenta">magenta</option>
       <option value="yellow">yellow</option>
-      <option value="black">black</option>
-      <option value="gray">gray</option>
-      <option value="white">white</option>
-      <option value="darkgray">darkgray</option>
-      <option value="lightgray">lightgray</option>
       <option value="brown">brown</option>
       <option value="lime">lime</option>
       <option value="olive">olive</option>
